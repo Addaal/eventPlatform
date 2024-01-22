3fa9fe9995803880e7acbc948b09da69cf580795
Addaal <adalmontania@gmail.com>
2024-01-22 09:52:16 -0300

    no webhook

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,6 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
+// import { WebhookEvent } from '@clerk/nextjs/server'
+import type { WebhookEvent } from "@clerk/clerk-sdk-node"
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
 import { clerkClient } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
@@ -8,8 +9,9 @@ import { NextResponse } from 'next/server'
 export async function POST(req: Request) {
  
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
- 
+  // const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+  const WEBHOOK_SECRET = "";
+
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
@@ -53,9 +55,10 @@ export async function POST(req: Request) {
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
-  
+
   console.log(eventType);
-  
+ 
+
   if(eventType === 'user.created') {
     console.log("webhook")
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
