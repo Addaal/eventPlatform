9418417ecb1776143072908531e114738f2623ef
Addaal <adalmontania@gmail.com>
2024-01-24 11:36:16 -0300

    trying to fix user create

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -70,7 +70,8 @@ export async function POST(req: Request) {
     }
 
     const newUser = await createUser(user);
-
+    console.log("new user created")
+    console.log(newUser);
     if(newUser) {
       await clerkClient.users.updateUserMetadata(id, {
         publicMetadata: {
